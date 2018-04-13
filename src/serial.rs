43cbb6e396987b6d0eff569819b5d09655afed7d
Teo Klestrup Röijezon <teo@nullable.se>
2018-04-13 11:41:31 +0200

    USART!

--- src/serial.rs
+++ src/serial.rs
@@ -5,14 +5,11 @@ use core::marker::PhantomData;
 
 use hal::serial;
 use nb;
-use stm32f0x0::{USART1, USART2, USART3};
+use stm32f0x0::{USART1, USART2, USART3, USART4, USART5};
 
-use gpio::gpioa::{PA10, PA2, PA3, PA9};
-use gpio::gpiob::{PB10, PB11, PB6, PB7};
-use gpio::gpioc::{PC10, PC11, PC4, PC5};
-use gpio::gpiod::{PD5, PD6, PD8, PD9};
-use gpio::gpioe::{PE0, PE1, PE15};
-use gpio::AF7;
+use gpio::gpioa::{PA2, PA3, PA9, PA10, PA14, PA15};
+use gpio::gpiob::{PB6, PB7};
+use gpio::{AF0, AF1};
 use rcc::{APB1, APB2, Clocks};
 use time::Bps;
 
@@ -45,34 +42,15 @@ pub unsafe trait TxPin<USART> {}
 /// RX pin - DO NOT IMPLEMENT THIS TRAIT
 pub unsafe trait RxPin<USART> {}
 
-unsafe impl TxPin<USART1> for PA9<AF7> {}
-unsafe impl TxPin<USART1> for PB6<AF7> {}
-unsafe impl TxPin<USART1> for PC4<AF7> {}
-unsafe impl TxPin<USART1> for PE0<AF7> {}
+unsafe impl TxPin<USART1> for PA2<AF1> {}
+unsafe impl TxPin<USART1> for PA9<AF1> {}
+unsafe impl TxPin<USART1> for PA14<AF1> {}
+unsafe impl TxPin<USART1> for PB6<AF0> {}
 
-unsafe impl RxPin<USART1> for PA10<AF7> {}
-unsafe impl RxPin<USART1> for PB7<AF7> {}
-unsafe impl RxPin<USART1> for PC5<AF7> {}
-unsafe impl RxPin<USART1> for PE1<AF7> {}
-
-unsafe impl TxPin<USART2> for PA2<AF7> {}
-// unsafe impl TxPin<USART2> for PA14<AF7> {}
-// unsafe impl TxPin<USART2> for PB3<AF7> {}
-unsafe impl TxPin<USART2> for PD5<AF7> {}
-
-unsafe impl RxPin<USART2> for PA3<AF7> {}
-// unsafe impl RxPin<USART2> for PA15<AF7> {}
-// unsafe impl RxPin<USART2> for PB4<AF7> {}
-unsafe impl RxPin<USART2> for PD6<AF7> {}
-
-unsafe impl TxPin<USART3> for PB10<AF7> {}
-unsafe impl TxPin<USART3> for PC10<AF7> {}
-unsafe impl TxPin<USART3> for PD8<AF7> {}
-
-unsafe impl RxPin<USART3> for PB11<AF7> {}
-unsafe impl RxPin<USART3> for PC11<AF7> {}
-unsafe impl RxPin<USART3> for PD9<AF7> {}
-unsafe impl RxPin<USART3> for PE15<AF7> {}
+unsafe impl RxPin<USART1> for PA3<AF1> {}
+unsafe impl RxPin<USART1> for PA10<AF1> {}
+unsafe impl RxPin<USART1> for PA15<AF1> {}
+unsafe impl RxPin<USART1> for PB7<AF0> {}
 
 /// Serial abstraction
 pub struct Serial<USART, PINS> {
@@ -237,7 +215,9 @@ macro_rules! hal {
 }
 
 hal! {
-    USART1: (usart1, APB2, usart1en, usart1rst, pclk2),
+    USART1: (usart1, APB2, usart1en, usart1rst, pclk1),
     USART2: (usart2, APB1, usart2en, usart2rst, pclk1),
     USART3: (usart3, APB1, usart3en, usart3rst, pclk1),
+    USART4: (usart4, APB1, usart4en, usart4rst, pclk1),
+    USART5: (usart5, APB1, usart5en, usart5rst, pclk1),
 }
